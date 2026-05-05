85879cae634c9427092616c14e53d89f54977d4a
KHUSHIGUP07 <khushigup9389588033@gmail.com>
2026-05-06 01:06:44 +0530

    fix chatlogics import path

--- src/components/SingleChat.js
+++ src/components/SingleChat.js
@@ -4,7 +4,7 @@ import { Input } from "@chakra-ui/input";
 import { Box, Text } from "@chakra-ui/layout";
 import "./styles.css";
 import { IconButton, Spinner, useToast } from "@chakra-ui/react";
-import { getSender, getSenderFull } from "../apiConfig.js/ChatLogics";
+import { getSender, getSenderFull } from "../apiConfig/ChatLogics";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { ArrowBackIcon } from "@chakra-ui/icons";
